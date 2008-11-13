48a323fc753fd3db8db6b12598e9aa5d4c043986
dbr <dbr.onix@gmail.com>
2008-11-13 21:36:41 +1030

    Added example site

---
+++ example.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+#encoding:utf-8
+"""Example usage of GET decorator.
+An hugely imagaintive blog site example.
+Posts are hard-coded ino the self.posts dict,
+get_post returns a specified post
+"""
+
+from pyerweb import GET, runner
+
+TEMPLATE = """<html>
+<head>
+<title>%(title)s</title>
+</head>
+<body>
+%(body)s
+</body>
+</html>"""
+
+class MySite:
+    """Main website functionality.
+    With a real site, the get/delete/edit/list posts would actually
+    persist the data somewhere (a database most likely)."""
+    
+    def __init__(self):
+        self.posts = {
+            'hello':"This is a simple post",
+            'another':"This is another post"
+        }
+
+    def get_post(self, postkey):
+        """This would query the database for postkey"""
+        return self.posts[postkey]
+    
+    def list_posts(self):
+        ret = ""
+        ret += "<ul>\n"
+        for cur_post in self.posts.keys():
+            ret += "<li>%s</li>\n" % (cur_post)
+        ret += "</ul>"
+        return ret
+    
+    def delete_post(self, postkey):
+        del self.posts[postkey]
+#end MySite
+
+# Here are the decorated functions that map URLs to
+# actions. For example, a GET request to "/" will go to
+# the index function, as the regex matches "/"
+# A GET request to "/blog/view/hello" will go to view_post.
+# A GET request to "/a/fake/URL" will return an error 404
+
+@GET("^/$")
+def index():
+    site = MySite()
+    
+    return TEMPLATE % {
+        'title': "Welcome to Test Blog",
+        'body': site.list_posts()
+    }
+
+@GET("^/blog/view/(.+?)$")
+def view_post(postkey):
+    site = MySite()
+    
+    return TEMPLATE % {
+        'title': '%s' % (postkey),
+        'body': site.get_post(postkey)
+    }
+
+@GET("/admin/post/(edit|delete)/(.+?)$")
+def admin_post(action, postkey):
+    site = MySite()
+    site.delete_post(postkey)
+    return "Performing admin action %s with post %s" % (action, postkey)
+
+
+# Siple dispatcher that uses fixed URLs. Will obviously be replaced by
+# a CGI script, or a simple web-server
+for request_url in ["/",
+                    "/blog/view/hello",
+                    "/admin/post/edit/hello",
+                    "/makeanerror404"]:
+    print "*"*15, "Request for", request_url, "*"*15
+    runner(request_url, output_helper = "html_tidy")
