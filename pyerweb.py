93f799458227241e51c2c8ec855cb59ca1ecdd3d
dbr <dbr.onix@gmail.com>
2009-01-14 21:41:42 +1030

    Inital code for WSGI compatibilty

--- pyerweb.py
+++ pyerweb.py
@@ -40,11 +40,15 @@ class GET:
                 # It's a direct call to the function
                 return func(*args, **kwargs)
         
-        PYERWEB_SITE_FUNCTIONS.append(new_fun) # Add to list of URL-mappable functions
+        PYERWEB_SITE_FUNCTIONS.setdefault(self.__class__.__name__, []).append(new_fun) # Add to list of URL-mappable functions
         return new_fun
 
-def router(url):
-    for cur in PYERWEB_SITE_FUNCTIONS:
+class PUT(GET):
+    pass
+
+def router(url, method = "GET"):
+    if method not in PYERWEB_SITE_FUNCTIONS.keys(): raise Pyerweb_PageNotFound
+    for cur in PYERWEB_SITE_FUNCTIONS[method]:
         try:
             output_html = cur(url)
             if not (isinstance(output_html, str) or isinstance(output_html, unicode)):
@@ -53,26 +57,40 @@ def router(url):
             pass # The regex doesn't match, skip it
         
         else:
-            return output_html
+            return (output_html, [("Content-type", "text/html")])
     else: # We checked all site functions, none matched, raise error 404
         raise Pyerweb_PageNotFound
 
-def runner(url, development = False, output_helper = None):
-    url = magic_url(url) # Wrap URL string into magic_url class
+def runner(environ, wsgi_start_response, url = None, method = None, output_helper = None):
+    """WSGI request handler"""
+    if environ is not None: # It's a WSGI call
+        method = environ['REQUEST_METHOD']
+        url = environ['PATH_INFO']
+    else: # runner("/the/url") call
+        method = "GET" if method is None else method
+        url = magic_url(url) # Wrap URL string into magic_url class
     # If the call to @GET decorator __call__() is a magic_url then
     # then the request came from here, if not it's a direct call!
     
+    output_headers = [("Content-type", "text/html")] # Default header
     try:
-        output_html = router(url)
+        output_html, output_headers = router(url, method = method)
     except Pyerweb_PageNotFound:
+        output_response = '404 Not Found'
         output_html = "<html><head><title>ERROR 404</title></head><body>The URL %s could not be found</body></html>" % (url)
     except Exception: # unhandled error!
+        output_response = '500 Internal Server Error'
         tb = traceback.format_exc()
         output_html = "<html><head><title>ERROR 500</title></head><body><p>Internal server error! The following error occured:</p><pre>%s</pre></body></html>" % (tb)
+    print "I GOT OUTPUT!"
+    print output_html
+    print output_headers
+    # if output_helper is not None:
+    #     if output_helper not in dir(OutputHelpers()):
+    #         raise ValueError("Invalid OutputHelper %s used" % (output_helper))
+    #     output_html = getattr(OutputHelpers(), output_helper)(output_html)
 
-    if output_helper is not None:
-        if output_helper not in dir(OutputHelpers()):
-            raise ValueError("Invalid OutputHelper %s used" % (output_helper))
-        output_html = getattr(OutputHelpers(), output_helper)(output_html)
-    
-    print(output_html)
+    if output_html is None:
+        output_response = "204 No Content"
+    wsgi_start_response("200 OK", output_headers)
+    return output_html
